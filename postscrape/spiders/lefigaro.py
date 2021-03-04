f4d8539eeccccad48fc73d7952fe3f098c39f10d
ralexy <44032920+ralexy@users.noreply.github.com>
2021-03-04 17:35:59 +0100

    Update lefigaro.py

--- postscrape/spiders/lefigaro.py
+++ postscrape/spiders/lefigaro.py
@@ -1,28 +1,37 @@
 import scrapy
 import json
+import dateparser
+import sys
+import re
 
 class LefigaroSpider(scrapy.Spider):
     name = 'lefigaro'
-    #allowed_domains = ['sport24.lefigaro.fr']
-    start_urls = [
-        'https://sport24.lefigaro.fr/football/ligue-1/24/2020/calendrier-resultats',
-    ]
+    start_urls = []
 
-    def buildStartUrls(self, response):
-        """Fonction chargée de récupérer les URL des résultats sportifs de toute une saison"""
+    def __init__(self, category='', base_url = 'https://sport24.lefigaro.fr/football/ligue-1/24/2020/calendrier-resultats', days = '1', **kwargs):
+        """ Fonction init utile pour scraper plusieurs jours (passés en argument) ou un autre sport du même site """
+        if int(days) > 1 :
+            for i in range(1, int(days)) :
+                self.start_urls.append(base_url + '?journee=' + str(i))
+        else :
+            self.start_urls.append(base_url + '?journee=' + days)
 
-        # Le dernier nb matché correspond à la dernière journée en cours de la compétition
-        max = response.xpath('//h1//span/text()').re(r"[1-9][0-9]?")
-        start_url = self.start_urls[0]
-        self.start_urls = []
 
-        for i in range(1, int(max[-1])) :
-            self.start_urls.append(start_url + '?journee='+ str(i))
+        print(self.start_urls)
+        print("Initialisation effectuée")
+
+        super().__init__(**kwargs)
 
     def parse(self, response):
         """Main function that parses the pages"""
+        print("Parsing en cours")
 
-        self.buildStartUrls(response)
+        # Extraction des jours de compétition de la page
+        parsedDays = self.cleanup(response.xpath('//div[@class="s24ls__slice"]//div//div[@class="s24ls-h3__content"]/text()').extract())
+
+        #print(parsedDays)
+        #print(dateparser.parse('samedi 22 août 2020').date())
+        #print("taille" + str(len(parsedDays)))
 
         results1 = response.xpath('//div[@class="s24ls-bloc__body"]//div//i[1]/text()').extract()
         results2 = response.xpath('//div[@class="s24ls-bloc__body"]//div//i[3]/text()').extract()
@@ -32,20 +41,44 @@ class LefigaroSpider(scrapy.Spider):
 
         status   = response.xpath('//span[@class="s24ls-bloc__status"]/text()').extract() # OK
 
-        print(json.dumps(results1))
-        print(json.dumps(self.start_urls))
+        date   = [None] * len(results1) # On remplit de valeurs vides notre tableau du jour
 
-        # Pages * enregistrements par page
-        tab = [len(status)]
-        for i in range (0, len(status)) :
-            line = {} # ligne courante = dictionnaire
+        # Accumulateur pour insérer la date ensuite (assez bizarre comme fonctionnement mais nécessaire au vu de la structure du site à scraper)
+        k = 0
 
-            line["status"]  = status[i].strip()
+        # On parcourt les jours
+        for i in range(0, len(parsedDays)) :
+            # On convertit la date littérale française en date normalisée
+            parsedDate = str(dateparser.parse(parsedDays[i]).date())
 
-            line["team1"]   = teams1[i]
-            line["team2"]   = teams2[i]
+            # On récupère le nb de matchs de ce jour
+            matchs = response.xpath('//div[@class="s24ls__slice"]['+ str(i+2) +']//div[@class="s24ls-bloc__body"]').extract()
 
-            line["result1"] = results1[i]
-            line["result2"] = results2[i]
+            #print("Parsing : ", parsedDate)
+            #print("Nb matchs du jour : ", len(matchs))
 
-            print(json.dumps(line))
+            # On boucle et on complète notre tableau
+            for j in range(0, len(matchs)) :
+                date[k] = parsedDate
+                k += 1
+
+        #print(date)
+
+        # On génère du JSON pour la sortie
+        for i in range (0, len(results1)) :
+            yield {
+                "team1": teams1[i],
+                "team2": teams2[i],
+                "results1": results1[i],
+                "results2": results2[i],
+                "status": status[i].strip(),
+                "day": date[i],
+            }
+
+    def cleanup(self, list) :
+        """Removes \n & \t in list of strings"""
+        res = []
+        for sub in list:
+            res.append(sub.replace("\n", "").strip())
+
+        return res
